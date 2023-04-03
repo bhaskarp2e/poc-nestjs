1b2371e47b0230a10bd919b608333ff499b421d1
bhaskarp2e <bhaskar.behl@p2epl.com>
2023-04-03 10:21:46 +0530

    adding valid pipe

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ValidationPipe} from "@nestjs/common";
+// import {ValidationPipe} from "@nestjs/common";
 import { AllExceptionsFilter } from './common/filters/expectations-filter';
+import { ValidationPipe } from './common/pipes/validation.pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
-  app.useGlobalPipes(new ValidationPipe({whitelist:true}));
+  // app.useGlobalPipes(new ValidationPipe({whitelist:true}));
+  
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(4001);
 }
 bootstrap();
